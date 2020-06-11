6949cf5d5de5629eba1d6dad895be33e08314440
Andrew Hulme <andrewhulme1993@gmail.com>
2020-06-11 17:11:58 +0100

    Adds CSS to the feed

--- src/components/feed.jsx
+++ src/components/feed.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Post from "./post";
+import "../feed.css";
 
 class Feed extends Component {
   constructor(props) {
@@ -52,9 +53,38 @@ class Feed extends Component {
             //   message={post.message}
             // />
 
-            <div className="postbox" key={index}>
-              <div className="info">Username: {post.user.username}</div>
-              <div className="message">Message: {post.message}</div>
+            // <div className="postbox" key={index}>
+            //   <div className="info">Username: {post.user.username}</div>
+            //   <div className="message">Message: {post.message}</div>
+            // </div>
+
+            <div className="card gedf-card">
+              <div class="card-header">
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="mr-2">
+                      <img
+                        className="rounded-circle"
+                        width="45"
+                        src="https://picsum.photos/50/50"
+                        alt=""
+                      ></img>
+                    </div>
+                    <div className="ml-2">
+                      <div className="h5 m-0">@{post.user.username}</div>
+                    </div>
+                  </div>
+                  <div></div>
+                </div>
+              </div>
+              <div className="card-body">
+                <div className="text-muted h7 mb-2">
+                  {" "}
+                  <i className="fa fa-clock-o"></i>15 min ago
+                </div>
+                <a className="card-link" href="#"></a>
+                <p className="card-text">{post.message}</p>
+              </div>
             </div>
           ))}
         </div>
