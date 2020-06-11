069310cd72905318cf364e0906fef3a4cad2d909
Andrew Hulme <andrewhulme1993@gmail.com>
2020-06-11 16:57:41 +0100

    Post box clears on submission

--- src/components/newPost.jsx
+++ src/components/newPost.jsx
@@ -19,6 +19,8 @@ class NewPost extends Component {
 
   handleSubmit = (evt) => {
     evt.preventDefault();
+    evt.target.reset();
+
     fetch(`https://acebook-backend.herokuapp.com/new`, {
       method: "POST",
       headers: {
@@ -60,3 +62,73 @@ class NewPost extends Component {
 }
 
 export default NewPost;
+
+{
+  /* <div className="card gedf-card">
+        <div class="card-header">
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="mr-2">
+                <img
+                  className="rounded-circle"
+                  width="45"
+                  src="https://picsum.photos/50/50"
+                  alt=""
+                ></img>
+              </div>
+              <div className="ml-2">
+                <div className="h5 m-0">@{this.state.username}</div>
+              </div>
+            </div>
+            <div>
+              <div className="dropdown">
+                <button
+                  className="btn btn-link dropdown-toggle"
+                  type="button"
+                  id="gedf-drop1"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  <i className="fa fa-ellipsis-h"></i>
+                </button>
+                <div
+                  className="dropdown-menu dropdown-menu-right"
+                  aria-labelledby="gedf-drop1"
+                >
+                  <div className="h6 dropdown-header">Configuration</div>
+                  <a className="dropdown-item" href="#">
+                    Save
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Hide
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Report
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card-body">
+          <div className="text-muted h7 mb-2">
+            {" "}
+            <i className="fa fa-clock-o"></i>15 min ago
+          </div>
+          <a className="card-link" href="#"></a>
+          <p className="card-text">{this.state.message}</p>
+        </div>
+        <div className="card-footer">
+          <a href="#" className="card-link">
+            <i className="fa fa-gittip"></i> Like
+          </a>
+          <a href="#" className="card-link">
+            <i className="fa fa-comment"></i> Comment
+          </a>
+          <a href="#" className="card-link">
+            <i className="fa fa-mail-forward"></i> Share
+          </a>
+        </div>
+      </div> */
+}
