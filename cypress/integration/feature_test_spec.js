707842b4adf5c47bcdd139027747f80842f37c54
@willhead <60652390+will-head@users.noreply.github.com>
2020-06-12 15:03:43 +0100

    Adds post and delete to Feature Test

--- cypress/integration/feature_test_spec.js
+++ cypress/integration/feature_test_spec.js
@@ -17,11 +17,28 @@ describe('Feature Test', () => {
     // cy.get('input[name="email"]').type(email);
     // cy.get('input[name="password"]').type(password);
     // cy.get('input[name="password_confirmation"]').type(password);
-    // cy.contains('Submit').click()
+    // cy.contains('Submit').click();
 
     cy.contains('Login').click()
-    cy.get('input[name="email"]').type(email);
+    cy.get('input[name="username"]').type(username);
     cy.get('input[name="password"]').type(password);
+    cy.contains('Submit').click().pause();
 
+    cy.visit('/')
+    cy.get('textarea[name="message"]').type(`I am number ${random}`)
+    cy.contains('Post').click().pause();;
+
+    // cy.visit('/')
+    // cy.contains('Edit').click();
+    // cy.get('textarea[name="message"]').type(`I am not a number, I am a free robot!`)
+    // cy.contains('Save').click().pause();
+
+    cy.visit('/')
+    cy.contains('Delete').click().pause();
+
+    cy.visit('/')
+    cy.contains('Sign Out').click().pause();
+
+//
   })
 })
