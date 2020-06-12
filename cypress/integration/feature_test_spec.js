1b6de98fe966b392049d3313152abcede29b455a
@willhead <60652390+will-head@users.noreply.github.com>
2020-06-12 14:30:15 +0100

    Adds Cypress and Feature test for Signup

---
+++ cypress/integration/feature_test_spec.js
@@ -0,0 +1,27 @@
+describe('Feature Test', () => {
+  it('Visits Acebook', () => {
+
+    // const random = Math.floor(Math.random() * 1000) + 1;
+    const random = 817
+    const username = `username${random}`;
+    const name = `Person ${random}`;
+    const email = `person${random}@example.com`;
+    const password = `12345`
+
+    cy.visit('/')
+
+    // cy.contains('Signup').click()
+    
+    // cy.get('input[name="username"]').type(username);
+    // cy.get('input[name="name"]').type(name);
+    // cy.get('input[name="email"]').type(email);
+    // cy.get('input[name="password"]').type(password);
+    // cy.get('input[name="password_confirmation"]').type(password);
+    // cy.contains('Submit').click()
+
+    cy.contains('Login').click()
+    cy.get('input[name="email"]').type(email);
+    cy.get('input[name="password"]').type(password);
+
+  })
+})
